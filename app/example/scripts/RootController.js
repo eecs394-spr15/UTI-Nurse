00efc5c96ae441a3da89a12714c8745189c4a045
brownrout <ericbrownrout2016@u.northwestern.edu>
2015-05-14 12:17:53 -0500

    Update Nurse view to view user submissions

--- app/example/scripts/RootController.js
+++ app/example/scripts/RootController.js
@@ -17,4 +17,23 @@ angular
           }
         });
   	};
+
+
+     $scope.checkUserSubmitted= function(){
+          var query = new Parse.Query(Parse.User);
+          query.equalTo("submitted", true);
+          query.find({
+            success: function(results) {
+              alert("Successfully retrieved " + results.length + " users.");
+              // Do something with the returned Parse.Object values
+              for (var i = 0; i < results.length; i++) { 
+                var object = results[i];
+                alert(object.id + ' - ' + object.get('username'));
+              }
+            },
+            error: function(error) {
+              alert("Error: " + error.code + " " + error.message);
+            }
+          });
+      };
   });
