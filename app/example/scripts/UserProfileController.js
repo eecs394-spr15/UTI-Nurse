c383fba6c8d021a6d5e2a229af150c7193d4a039
Strick4888 <benjaminstrick2016@u.northwestern.edu>
2015-05-17 20:38:00 -0500

    added nurse ability to suggest clinic vist instead of authorize

--- app/example/scripts/UserProfileController.js
+++ app/example/scripts/UserProfileController.js
@@ -7,7 +7,11 @@ angular
     $scope.authorize =function(){
 supersonic.ui.dialog.alert("Authorized");
     }
-    
+
+    $scope.suggestClinicVisit =function(){
+supersonic.ui.dialog.alert("Suggested");
+    }
+
     supersonic.ui.views.current.params.onValue( function (values) {
       //alert(values.id);
       $scope.userProfile = JSON.parse(values.id);
