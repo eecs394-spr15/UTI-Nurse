6bc5105fd9f05445642dd648b9b34f7202e1093d
Strick4888 <benjaminstrick2016@u.northwestern.edu>
2015-05-17 20:16:47 -0500

    added authorize button for nurses

--- app/example/scripts/UserProfileController.js
+++ app/example/scripts/UserProfileController.js
@@ -3,6 +3,11 @@ angular
   .controller('UserProfileController', function($scope, supersonic) {
    	$scope.userProfile = undefined;
 
+
+    $scope.authorize =function(){
+supersonic.ui.dialog.alert("Authorized");
+    }
+    
     supersonic.ui.views.current.params.onValue( function (values) {
       //alert(values.id);
       $scope.userProfile = JSON.parse(values.id);
